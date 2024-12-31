58b9ab5c48a8b4dd871a97adfd620dd1480b1ac1
christopherganda <christopher.ganda@gmail.com>
2024-12-31 15:12:56 +0700

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,21 @@
+class User < ApplicationRecord
+  # Get a user's followings' id
+  def following_ids
+    UserFollow.where(follower_id: id).pluck(:followee_id)
+  end
+
+  # Get a user's followers' id
+  def follower_ids
+    UserFollow.where(followee_id: id).pluck(:follower_id)
+  end
+
+  # Get a user's followings' information
+  def followings
+    User.where(id: followee_ids)
+  end
+
+  # Get a user's followers' information
+  def followers
+    User.where(id: follower_ids)
+  end
+end

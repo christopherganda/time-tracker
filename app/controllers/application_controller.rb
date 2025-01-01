cb3ca93290000b0d4e8c8319f7f64cb447d91c7e
christopherganda <christopher.ganda@gmail.com>
2025-01-01 18:23:35 +0700

    Set model to optional and use locales error messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+  def render_error(status, messages)
+    render json: {
+      error: messages,
+    }, status: status
+  end
+
+  def render_success_no_data(message)
+    render json: {
+      message: message,
+    }, status: :ok
+  end
 end

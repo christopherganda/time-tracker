6b00b25c7dd4901a93e1e7ee3c304afe409a9ca3
Christopher ganda <christopher.ganda@gmail.com>
2025-01-02 11:53:20 +0700

    Tidy up the code (#8)
    * Tidy up clock ins controller using user finder and unit test
    
    * tidy up user follows controller with unit test
    
    * tidy up unit test and use factory bot

--- spec/controllers/user_follows_controller_spec.rb
+++ spec/controllers/user_follows_controller_spec.rb
@@ -1,61 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe UserFollowsController, type: :controller do
-  let!(:user1) { User.create!(name: "test1") }
-  let!(:user2) { User.create!(name: "test2") }
+  let!(:user1) { create(:user) }
+  let!(:user2) { create(:user) }
 
-  describe "POST #create" do
-    context "when actor_id does not exist" do
-      it "returns not found error" do
-        post :create, params: { actor: 100, followee_id: 100 }
+  describe 'POST #create' do
+    context 'when actor_id does not exist' do
+      it 'returns not found error' do
+        post :create, params: { actor: nil, followee_id: nil }
         expect(response).to have_http_status(:not_found)
-        expect(JSON.parse(response.body)["error"]).to include("Actor does not exist")
+        expect(JSON.parse(response.body)['error']).to include('Actor does not exist')
       end
     end
 
-    context "when the followee does not exist" do
-      it "returns not found error" do
-        post :create, params: { actor: user1.id, followee_id: 100 }
+    context 'when the followee does not exist' do
+      it 'returns not found error' do
+        post :create, params: { actor: user1.id, followee_id: nil }
         expect(response).to have_http_status(:not_found)
-        expect(JSON.parse(response.body)["error"]).to eq("Followee does not exist")
+        expect(JSON.parse(response.body)['error']).to eq('Followee does not exist')
       end
     end
 
-    context "success" do
-      it "creates record to UserFollow" do
-        post :create, params: { actor: user1.id, followee_id: user2.id }
+    context 'success' do
+      it 'creates record to UserFollow' do
+        expect {
+          post :create, params: { actor: user1.id, followee_id: user2.id }
+        }.to change(UserFollow, :count).by(1)
         expect(response).to have_http_status(:ok)
         expect(UserFollow.exists?(follower_id: user1.id, followee_id: user2.id)).to be_truthy
       end
     end
 
-    context "when the user tries to follow themselves" do
-      it "returns internal server error" do
+    context 'when the user tries to follow themselves' do
+      it 'returns internal server error' do
         post :create, params: { actor: user1.id, followee_id: user1.id }
         expect(response).to have_http_status(:internal_server_error)
-        expect(JSON.parse(response.body)["error"]).to include("Follower cannot be the same as Followee")
+        expect(JSON.parse(response.body)['error']).to include('Follower cannot be the same as Followee')
       end
     end
   end
 
-  describe "DELETE #destroy" do
-    context "success" do
+  describe 'DELETE #destroy' do
+    context 'success' do
       before do
         UserFollow.create(follower_id: user1.id, followee_id: user2.id)
       end
 
-      it "delete record from UserFollow" do
-        delete :destroy, params: { actor: user1.id, followee_id: user2.id }
+      it 'delete record from UserFollow' do
+        expect {
+          delete :destroy, params: { actor: user1.id, followee_id: user2.id }
+        }.to change(UserFollow, :count).by(-1)
         expect(response).to have_http_status(:ok)
         expect(UserFollow.exists?(follower_id: user1.id, followee_id: user2.id)).to be_falsey
       end
     end
 
-    context "record does not exist" do
-      it "return relationship does not exist" do
+    context 'record does not exist' do
+      it 'return relationship does not exist' do
         delete :destroy, params: { actor: user1.id, followee_id: user2.id }
         expect(response).to have_http_status(:not_found)
-        expect(JSON.parse(response.body)["error"]).to eq("Following relationship does not exist")
+        expect(JSON.parse(response.body)['error']).to eq('Following relationship does not exist')
       end
     end
   end
--- spec/models/clock_in_spec.rb
+++ spec/models/clock_in_spec.rb
@@ -1,12 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe ClockIn, type: :model do
-  let!(:user1) { User.create!(name: "test1") }
+  let!(:user1) { create(:user) }
   
-  context 'validations' do
-    it 'succeed' do
-      clock_in = ClockIn.new(user_id: user1.id, clocked_in_at: Time.now.getutc, is_clocked_out: false)
-      expect(clock_in).to be_valid
-    end
+  it 'succeed' do
+    clock_in = ClockIn.new(user_id: user1.id, clocked_in_at: Time.now, is_clocked_out: false)
+    expect(clock_in).to be_valid
   end
 end

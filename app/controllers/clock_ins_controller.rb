f0831c2518a3a6cfacef3907ded71151e9b3bbd9
christopherganda <christopher.ganda@gmail.com>
2025-01-03 09:05:56 +0700

    add retry mechanism on clock ins to minimize deadlock

--- app/controllers/clock_ins_controller.rb
+++ app/controllers/clock_ins_controller.rb
@@ -1,14 +1,16 @@
 class ClockInsController < ApplicationController
   include UserFinder
+  include Retryable
 
   def upsert
     current_time = Time.now
 
-    begin
+    with_retry do
       ActiveRecord::Base.transaction do
         last_unused_clock_in = ClockIn.where(
-          user_id: @user.id, 
-          is_clocked_out: false)
+          is_clocked_out: false,
+          user_id: @user.id
+          )
           .order(clocked_in_at: :desc)
           .limit(1)
           .lock(true)
@@ -27,14 +29,12 @@ class ClockInsController < ApplicationController
         else
           ClockIn.create!(
             user_id: @user.id,
-            clocked_in_at: Time.now
+            clocked_in_at: current_time
           )
 
           render_success(@user.clock_ins_json, I18n.t('success.messages.clock_in_success'))
         end
       end
-    rescue ActiveRecord::ActiveRecordError => e
-      render_error(:unprocessable_entity, e.message)
     end
   end
 
